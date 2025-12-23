9571b6e4ec864d2285730b02dcdf2d155b1e18be
André <andre_miguel_costa@hotmail.com>
2025-12-23 13:50:02 +0100

    feat: add initial sdl + egl integration

--- src/drivers/sdl/lv_sdl_window.c
+++ src/drivers/sdl/lv_sdl_window.c
@@ -18,6 +18,7 @@
 #include "../../core/lv_global.h"
 #include "../../display/lv_display_private.h"
 #include "../../lv_init.h"
+#include "../opengles/lv_opengles_driver.h"
 #include "../../draw/lv_draw_buf.h"
 #include "../../draw/nanovg/lv_draw_nanovg.h"
 #include "../../drivers/opengles/lv_opengles_driver.h"
@@ -62,39 +63,13 @@
 /**********************
  *      TYPEDEFS
  **********************/
-typedef struct {
-    SDL_Window * window;
-    SDL_Renderer * renderer;
-#if LV_USE_DRAW_SDL == 0
-    SDL_Texture * texture;
-    uint8_t * fb1;
-    uint8_t * fb2;
-    uint8_t * fb_act;
-    uint8_t * buf1;
-    uint8_t * buf2;
-    uint8_t * rotated_buf;
-    size_t rotated_buf_size;
-#endif
-    float zoom;
-    uint8_t ignore_size_chg;
-
-#if LV_SDL_USE_EGL
-    struct {
-        EGLDisplay display;
-        EGLSurface surface;
-        EGLContext context;
-        EGLConfig config;
-        void * gl_lib_handle;
-    } egl;
-#endif
-} lv_sdl_window_t;
 
 /**********************
  *  STATIC PROTOTYPES
  **********************/
 static inline int sdl_render_mode(void);
 static void flush_cb(lv_display_t * disp, const lv_area_t * area, uint8_t * color_p);
-static void window_create(lv_display_t * disp);
+static lv_result_t window_create(lv_display_t * disp);
 static void window_update(lv_display_t * disp);
 #if LV_USE_DRAW_SDL == 0
     static void texture_resize(lv_display_t * disp);
@@ -140,10 +115,15 @@ lv_display_t * lv_sdl_window_create(int32_t hor_res, int32_t ver_res)
         lv_free(dsc);
         return NULL;
     }
-    lv_display_add_event_cb(disp, release_disp_cb, LV_EVENT_DELETE, disp);
     lv_display_set_driver_data(disp, dsc);
-    window_create(disp);
-
+    lv_result_t res = window_create(disp);
+    if(res == LV_RESULT_INVALID) {
+        LV_LOG_ERROR("Failed to initialize window");
+        lv_free(dsc);
+        lv_display_delete(disp);
+        return NULL;
+    }
+    lv_display_add_event_cb(disp, release_disp_cb, LV_EVENT_DELETE, disp);
     lv_display_set_flush_cb(disp, flush_cb);
 
 #if LV_USE_DRAW_NANOVG
@@ -291,10 +271,14 @@ static void flush_cb(lv_display_t * disp, const lv_area_t * area, uint8_t * px_m
     lv_color_format_t cf = lv_display_get_color_format(disp);
     uint32_t * argb_px_map = NULL;
 
-#if LV_SDL_USE_EGL
+#if LV_USE_EGL
     /* EGL rendering path */
     if(lv_display_flush_is_last(disp)) {
-        eglSwapBuffers(dsc->egl.display, dsc->egl.surface);
+        lv_opengles_viewport(0, 0,
+                             lv_display_get_original_horizontal_resolution(disp),
+                             lv_display_get_horizontal_resolution(disp));
+        lv_opengles_render_display_texture(disp, false, true);
+        lv_opengles_egl_update(dsc->egl_ctx);
     }
     lv_display_flush_ready(disp);
     return;
@@ -429,146 +413,16 @@ static void sdl_event_handler(lv_timer_t * t)
     }
 }
 
-#if LV_SDL_USE_EGL
-static void * s_gl_lib_handle = NULL;
-
-static GLADapiproc glad_egl_load_cb(void * userptr, const char * name)
-{
-    LV_UNUSED(userptr);
-    GLADapiproc proc = NULL;
-
-    /* First try eglGetProcAddress for extension functions */
-    proc = (GLADapiproc)eglGetProcAddress(name);
-    if(proc) return proc;
-
-    /* Fall back to dlsym for core functions */
-    if(s_gl_lib_handle) {
-        proc = (GLADapiproc)dlsym(s_gl_lib_handle, name);
-    }
-    return proc;
-}
-
-static bool init_egl(lv_sdl_window_t * dsc)
-{
-    /* Load OpenGL ES library */
-    dsc->egl.gl_lib_handle = dlopen("libGLESv2.so", RTLD_LAZY);
-    if(!dsc->egl.gl_lib_handle) {
-        dsc->egl.gl_lib_handle = dlopen("libGLESv2.so.2", RTLD_LAZY);
-    }
-    if(!dsc->egl.gl_lib_handle) {
-        LV_LOG_ERROR("Failed to load OpenGL ES library: %s", dlerror());
-        return false;
-    }
-
-    dsc->egl.display = eglGetDisplay(EGL_DEFAULT_DISPLAY);
-    if(dsc->egl.display == EGL_NO_DISPLAY) {
-        LV_LOG_ERROR("Failed to get EGL display");
-        return false;
-    }
-
-    if(!eglInitialize(dsc->egl.display, NULL, NULL)) {
-        LV_LOG_ERROR("Failed to initialize EGL");
-        return false;
-    }
-
-    const EGLint config_attribs[] = {
-        EGL_RENDERABLE_TYPE, EGL_OPENGL_ES2_BIT,
-        EGL_SURFACE_TYPE, EGL_WINDOW_BIT,
-        EGL_BLUE_SIZE, 8,
-        EGL_GREEN_SIZE, 8,
-        EGL_RED_SIZE, 8,
-        EGL_ALPHA_SIZE, 8,
-        EGL_STENCIL_SIZE, 8,
-        EGL_SAMPLES, 4, /* 4x MSAA */
-        EGL_NONE
-    };
-
-    EGLint num_configs;
-    if(!eglChooseConfig(dsc->egl.display, config_attribs, &dsc->egl.config, 1, &num_configs)) {
-        LV_LOG_ERROR("Failed to choose EGL config");
-        return false;
-    }
-
-    SDL_SysWMinfo wmInfo;
-    SDL_VERSION(&wmInfo.version);
-    SDL_GetWindowWMInfo(dsc->window, &wmInfo);
-
-    EGLNativeWindowType native_window;
-#if defined(SDL_VIDEO_DRIVER_WINDOWS)
-    native_window = wmInfo.info.win.window;
-#elif defined(SDL_VIDEO_DRIVER_X11)
-    native_window = wmInfo.info.x11.window;
-#elif defined(SDL_VIDEO_DRIVER_WAYLAND)
-    native_window = wmInfo.info.wl.surface;
-#else
-    LV_LOG_ERROR("Unsupported platform for EGL");
-    return false;
-#endif
-
-    dsc->egl.surface = eglCreateWindowSurface(dsc->egl.display, dsc->egl.config, native_window, NULL);
-    if(dsc->egl.surface == EGL_NO_SURFACE) {
-        LV_LOG_ERROR("Failed to create EGL surface");
-        return false;
-    }
-
-    const EGLint context_attribs[] = {
-        EGL_CONTEXT_CLIENT_VERSION, 2,
-        EGL_NONE
-    };
-
-    dsc->egl.context = eglCreateContext(dsc->egl.display, dsc->egl.config, EGL_NO_CONTEXT, context_attribs);
-    if(dsc->egl.context == EGL_NO_CONTEXT) {
-        LV_LOG_ERROR("Failed to create EGL context");
-        return false;
-    }
-
-    if(!eglMakeCurrent(dsc->egl.display, dsc->egl.surface, dsc->egl.surface, dsc->egl.context)) {
-        LV_LOG_ERROR("Failed to make EGL context current");
-        return false;
-    }
-
-    /* Load GLES2 functions via GLAD */
-    s_gl_lib_handle = dsc->egl.gl_lib_handle;
-    if(!gladLoadGLES2UserPtr(glad_egl_load_cb, NULL)) {
-        LV_LOG_ERROR("Failed to load OpenGL ES2 functions via GLAD");
-        return false;
-    }
-
-    LV_LOG_INFO("OpenGL ES version: %s", glGetString(GL_VERSION));
-    LV_LOG_INFO("GLSL version: %s", glGetString(GL_SHADING_LANGUAGE_VERSION));
-    LV_LOG_INFO("GL extensions: %s", glGetString(GL_EXTENSIONS));
-
-    return true;
-}
-
-static void deinit_egl(lv_sdl_window_t * dsc)
-{
-    if(dsc->egl.display != EGL_NO_DISPLAY) {
-        eglMakeCurrent(dsc->egl.display, EGL_NO_SURFACE, EGL_NO_SURFACE, EGL_NO_CONTEXT);
-        if(dsc->egl.context != EGL_NO_CONTEXT) {
-            eglDestroyContext(dsc->egl.display, dsc->egl.context);
-        }
-        if(dsc->egl.surface != EGL_NO_SURFACE) {
-            eglDestroySurface(dsc->egl.display, dsc->egl.surface);
-        }
-        eglTerminate(dsc->egl.display);
-    }
-    if(dsc->egl.gl_lib_handle) {
-        dlclose(dsc->egl.gl_lib_handle);
-        dsc->egl.gl_lib_handle = NULL;
-    }
-    dsc->egl.display = EGL_NO_DISPLAY;
-    dsc->egl.context = EGL_NO_CONTEXT;
-    dsc->egl.surface = EGL_NO_SURFACE;
-}
-#endif
-
-static void window_create(lv_display_t * disp)
+static lv_result_t window_create(lv_display_t * disp)
 {
     lv_sdl_window_t * dsc = lv_display_get_driver_data(disp);
     dsc->zoom = 1.0;
 
     int flag = 0;
+#if LV_USE_EGL
+    flag |= SDL_WINDOW_OPENGL;
+#endif
+
 #if LV_SDL_FULLSCREEN
     flag |= SDL_WINDOW_FULLSCREEN;
 #endif
@@ -583,14 +437,17 @@ static void window_create(lv_display_t * disp)
                                    SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED,
                                    hor_res, ver_res, flag);       /*last param. SDL_WINDOW_BORDERLESS to hide borders*/
 
-#if LV_SDL_USE_EGL
-    if(!init_egl(dsc)) {
-        LV_LOG_ERROR("Failed to initialize EGL, falling back to SDL renderer");
+#if LV_USE_EGL
+    lv_egl_interface_t ifc = lv_sdl_get_egl_interface(disp);
+    dsc->egl_ctx = lv_opengles_egl_context_create(&ifc);
+    if(!dsc->egl_ctx) {
+        LV_LOG_ERROR("Failed to initialize EGL context");
+        return LV_RESULT_INVALID;
     }
 #else
     dsc->renderer = SDL_CreateRenderer(dsc->window, -1,
                                        LV_SDL_ACCELERATED ? SDL_RENDERER_ACCELERATED : SDL_RENDERER_SOFTWARE);
-#endif /*LV_SDL_USE_EGL*/
+#endif /*LV_USE_EGL*/
 
 #if LV_USE_DRAW_SDL == 0
     texture_resize(disp);
@@ -605,6 +462,7 @@ static void window_create(lv_display_t * disp)
 #if LV_USE_DRAW_SDL == 0
     texture_resize(disp);
 #endif /*LV_USE_DRAW_SDL == 0*/
+    return LV_RESULT_OK;
 }
 
 static void window_update(lv_display_t * disp)
@@ -727,8 +585,8 @@ static void release_disp_cb(lv_event_t * e)
 #if LV_USE_DRAW_SDL == 0
     SDL_DestroyTexture(dsc->texture);
 #endif
-#if LV_SDL_USE_EGL
-    deinit_egl(dsc);
+#if LV_USE_EGL
+    lv_opengles_egl_context_destroy(dsc->egl_ctx);
 #endif
     if(dsc->renderer) {
         SDL_DestroyRenderer(dsc->renderer);
